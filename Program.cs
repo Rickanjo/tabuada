0ce5cb223a0416de06b2d0481f00d31286ed56e5
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-06-11 22:13:54 -0300

    Exerício Corrigido

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Digite um número inteiro: ");
+int numero = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"Tabuada do número: {numero}\n");
+
+for (int i = 0; i <= 10; i++)
+{
+    int resultado = numero * i;
+    Console.WriteLine($"{numero} x {i} = {resultado}");
+}
+
